f5f5ff1be4e19b91f46904232caba9d54affd74f
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-07-09 18:22:51 +0530

    update

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { Mail, Download, ChevronDown } from 'lucide-react';
 
-const resume = 'Nadun_dananjaya_CV.pdf';
+const resume = '_CV.pdf';
 // Use a string path for public assets in Next.js
 const propic = '/profile.webp';
 
